c56956d5e160ad1b1c869c6a5ce6531c61561a6f
SbMtuGit <genshindevelop@gmail.com>
2024-12-09 14:47:56 +0000

    new

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const ctrlLocations = require('../controllers/locations');  // Ensure this path is correct
-const Account = require('../models/Account');  // Assuming your Account model is in the 'models' folder
+const ctrlLocations = require('../controllers/locations');  
+const Account = require('../models/Account');  
 
-// Route to display homepage or the list of locations
+
 router.get('/', ctrlLocations.homelist);
 
-// Route to read a specific album by ID
+
 router.get('/location/Discover/:AlbumsID', ctrlLocations.AlbumReadOne);
 
-// Route to get all music (Discover page)
+
 router.get('/Discover', ctrlLocations.getAllMusic);
 
 // Register Route (POST request)
 router.post('/auth/register', (req, res, next) => {
     const { username, password, email } = req.body;
 
-    // Register a new account using the passport-local-mongoose method
+ 
     Account.register(new Account({ username, email }), password, (err, account) => {
         if (err) {
             return res.status(500).json({ message: 'Error creating account', error: err });
@@ -45,7 +45,7 @@ router.get('/auth/logout', (req, res, next) => {
             if (err) {
                 return next(err);
             }
-            res.redirect('/');  // Redirect to homepage or desired route after logout
+            res.redirect('/'); 
         });
     });
 });
